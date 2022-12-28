c0237462b52fd8daa8a9bac1bfbe46a065c6f660
leemhoon00 <leemhoon00@naver.com>
2022-12-29 00:38:27 +0900

    apply mui(not styles)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import logo from './logo.svg';
 import Customer from './components/Customer'
 import './App.css';
+
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+
+
+
 
 const customers =[
   {
@@ -32,23 +42,37 @@ const customers =[
 
 function App() {
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          )
-        })
-      }
-    </div>
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
