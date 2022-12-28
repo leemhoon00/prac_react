e1bc3512182a5ef0843e160033db01ef9a222f67
leemhoon00 <leemhoon00@naver.com>
2022-12-28 16:38:32 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers =[
+  {
+  'id':1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name' : '임종훈',
+  'birthday' : '000114',
+  'gender' : '남자',
+  'job' : '프로그래머'
+  },
+  {
+    'id':2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : '000124',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id':3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name' : '이순신',
+    'birthday' : '000134',
+    'gender' : '남자',
+    'job' : '디자이너'
+  }
+
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Lets develop management System!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
