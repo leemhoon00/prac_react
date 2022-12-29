f317b8c8142c032b17e2eca687d72e8cf35b1921
dlehdtjr13 <ehdtjrt1272@naver.com>
2022-12-29 16:27:54 +0900

    add get customers rest api module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Customer from './components/Customer'
 import './App.css';
 
+import React from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
@@ -10,70 +11,60 @@ import TableCell from '@mui/material/TableCell';
 
 
 
+class App extends React.Component {
 
+  state={
+    customers: ""
+  };
 
-const customers =[
-  {
-  'id':1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name' : '임종훈',
-  'birthday' : '000114',
-  'gender' : '남자',
-  'job' : '프로그래머'
-  },
-  {
-    'id':2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name' : '홍길동',
-    'birthday' : '000124',
-    'gender' : '남자',
-    'job' : '대학생'
-  },
-  {
-    'id':3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name' : '이순신',
-    'birthday' : '000134',
-    'gender' : '남자',
-    'job' : '디자이너'
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
-]
+  callApi = async() =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
 
-function App() {
-  return (
-    <Paper>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            )
-          })
-        }
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  render(){
+    return (
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+            this.state.customers ? this.state.customers.map(c => {
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              )
+            }) : ""
+          }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+  
 }
 
 export default App;
--- server.js
+++ server.js
@@ -4,8 +4,33 @@ const port = 5000;
 
 
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+            'id':1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name' : '임종훈',
+            'birthday' : '000114',
+            'gender' : '남자',
+            'job' : '프로그래머'
+            },
+            {
+              'id':2,
+              'image': 'https://placeimg.com/64/64/2',
+              'name' : '홍길동',
+              'birthday' : '000124',
+              'gender' : '남자',
+              'job' : '대학생'
+            },
+            {
+              'id':3,
+              'image': 'https://placeimg.com/64/64/3',
+              'name' : '이순신',
+              'birthday' : '000134',
+              'gender' : '남자',
+              'job' : '디자이너'
+            }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
