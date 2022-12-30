cc905024bef672bec70051ca6e94606aaf49b235
leemhoon00 <leemhoon00@naver.com>
2022-12-30 15:46:33 +0900

    create customer database table & node.js database connection Settings

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const fs = require('fs');
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
 
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
 
 app.get('/api/customers',(req,res)=>{
-    res.send([
-        {
-            'id':1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name' : '임종훈',
-            'birthday' : '000114',
-            'gender' : '남자',
-            'job' : '프로그래머'
-            },
-            {
-              'id':2,
-              'image': 'https://placeimg.com/64/64/2',
-              'name' : '홍길동',
-              'birthday' : '000124',
-              'gender' : '남자',
-              'job' : '대학생'
-            },
-            {
-              'id':3,
-              'image': 'https://placeimg.com/64/64/3',
-              'name' : '이순신',
-              'birthday' : '000134',
-              'gender' : '남자',
-              'job' : '디자이너'
-            }
-    ]);
+    connection.query(
+      "select * from customer",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
